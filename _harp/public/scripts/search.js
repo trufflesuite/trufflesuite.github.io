5afa0407a848095e8efbea04e2868aefd240e628
John McDowall <john@mcdowall.info>
2016-11-01 15:51:53 -0700

    Search box styling

--- _harp/public/scripts/search.js
+++ _harp/public/scripts/search.js
@@ -1,3 +1,16 @@
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+    }
+  }
+}
+
 (function() {
   function displaySearchResults(results, store) {
     var searchResults = document.getElementById('search-results');
@@ -17,20 +30,11 @@
     }
   }
 
-  function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
-
-      if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-      }
-    }
-  }
 
   var searchTerm = getQueryVariable('query');
+
+  var queryTermEl = document.getElementsByClassName('queryTerm');
+  queryTermEl[0].appendChild( document.createTextNode(searchTerm) );
 
   if (searchTerm) {
     document.getElementById('search-box').setAttribute("value", searchTerm);
